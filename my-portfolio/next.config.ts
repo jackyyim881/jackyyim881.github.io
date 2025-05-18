59129e9e9f14bc69292e0eda47dc1e3cfb94a63f
jackyyim881 <jackyyim881@hotmail.com>
2025-05-18 14:25:28 +0800

    update

--- my-portfolio/next.config.ts
+++ my-portfolio/next.config.ts
@@ -2,10 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
-  basePath: "/my-portfolio",
-  assetPrefix: "/my-portfolio/",
+  output: "standalone",
+
+  images: {
+    unoptimized: true,
+  },
+  // The trailingSlash option helps with path resolution in static exports
   trailingSlash: true,
+  // Make sure PostCSS (which Tailwind uses) runs properly
+  webpack: (config) => {
+    return config;
+  },
 };
 
 export default nextConfig;
