b197c5957e22e49785cfc10162eabf60b3b30970
jackyyim881 <jackyyim881@hotmail.com>
2025-05-19 16:58:40 +0800

    update

--- my-portfolio/next.config.ts
+++ my-portfolio/next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
-
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  assetPrefix: "./",
+  basePath: isProd ? "" : "",
+  assetPrefix: "/",
 
   images: {
     unoptimized: true,
